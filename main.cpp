4543b1b5c8bd023141359b9daf89f457c311f437
matozog <226125@student.pwr.edu.pl>
2018-05-11 09:45:46 +0100

    init main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <thread>
+#include <unistd.h>
+#include <ncurses.h>
+#include <mutex>
 
 using namespace std;
 
+void loadMapFromFile(int **mapArray);
+
+static string mapFileName = "map";
+
 int main() {
-    cout << "Hello, World!" << std::endl;
+
+    initscr();
+    nodelay(stdscr,TRUE);
+    start_color();
+
+    int **mapArray;
+    loadMapFromFile(mapArray);
+
+
+    endwin();
+
     return 0;
+}
+
+void loadMapFromFile(int **mapArray)
+{
+
 }
